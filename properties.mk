da4769228f76b3b39008e98e2580b5e97d688293
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    RMX1941: Set lmkd configuration properties
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- properties.mk
+++ properties.mk
@@ -63,5 +63,17 @@ PRODUCT_PRODUCT_PROPERTIES += \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera \
     vendor.camera.aux.packageblacklist=org.telegram.messenger,com.microsoft.teams,com.discord
 
+# LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=0 \
+    ro.lmk.critical=0 \ 
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \ 
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true \ 
+    ro.lmk.log_stats=true
 
 
