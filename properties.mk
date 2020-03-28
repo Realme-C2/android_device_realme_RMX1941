411fa32eed78a5f8b7bcc2ed43ac3ca885c2c01e
Alessandro Astone <ales.astone@gmail.com>
2020-03-28 12:09:07 +0100

    RMX1941: Disable SF backpressure
    Eliminates UI stutters and lags.
    
    Change-Id: I4d5e9ec52901a8b7da9c0c439417a4ac90c512b3
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- properties.mk
+++ properties.mk
@@ -37,7 +37,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # SurfaceFlinger
 PRODUCT_PRODUCT_PROPERTIES += \
-    debug.sf.enable_gl_backpressure=1 \
+    debug.sf.disable_backpressure=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
