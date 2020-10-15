a1dc7c74526fece6969a55ccf428bb89ac1fca09
Harshit Jain <god@hyper-labs.tech>
2020-10-16 00:44:49 +0530

    RMX1941: properties: add dex2oat config and enable dex2oat64 for performance
    Change-Id: I9d2ebb05800fdea92562b39ca8a74af177a9b285
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

--- properties.mk
+++ properties.mk
@@ -17,3 +17,16 @@
 # Charger
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.charger.enable_suspend=true
+
+# Dex
+PRODUCT_PRODUCT_PROPERTIES += \
+    dalvik.vm.dex2oat64.enabled=true \
+    ro.vendor.qti.am.reschedule_service=true \
+    ro.sys.fw.dex2oat_thread_count=8 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.dex2oat-filter=quicken \
+    dalvik.vm.dex2oat-threads=8 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-filter=quicken \
+    dalvik.vm.image-dex2oat-threads=8
