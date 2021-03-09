1d4deea0257107a8d3ea51e8515ea4f2a7856669
SparXFusion <s2234nadar@gmail.com>
2021-03-09 19:11:27 +0530

    RMX1941: initial skeleton tree
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>

---
+++ aosp_RMX1941.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2021 The Android Open Source Project 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit some common Aosp stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit from RMX1941 device makefile
+$(call inherit-product, device/realme/RMX1941/device.mk)
+
+# Dimen
+TARGET_SCREEN_HEIGHT := 1560
+TARGET_SCREEN_WIDTH := 720
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := RMX1941
+PRODUCT_NAME := aosp_RMX1941
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := Realme C2
+PRODUCT_MANUFACTURER := realme
+PRODUCT_RELEASE_NAME := Realme C2
+
---
+++ device.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2021 Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/realme/RMX1941
+
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 29
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# FSTAB
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/rootdir/etc/fstab.mt6765:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6765
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.target.rc \
+    fstab.mt6765 
+
+# Screen density
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
+
+
