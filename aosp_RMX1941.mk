4ebf6076d68ebf71e20a492c238b7f43cb1d0021
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2021-03-19 02:27:15 +0530

    RMX1941: use pixel sunfish march fp

--- aosp_RMX1941.mk
+++ aosp_RMX1941.mk
@@ -35,3 +35,5 @@ PRODUCT_MODEL := Realme C2
 PRODUCT_MANUFACTURER := realme
 PRODUCT_RELEASE_NAME := Realme C2
 
+# Build FP
+BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ2A.210305.006/7119741:user/release-keys"
