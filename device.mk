291fd39efb2dc4cb9ebb404fbf55a2e47eec13ee
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2021-03-18 11:24:37 +0530

    RMX1941: nuke updateable apex

--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ DEVICE_PATH := device/realme/RMX1941
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
